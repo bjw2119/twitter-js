468a2d9b3ba0145066712ea3dd03e1e235e2a587
Barry Weinberg <bjw2119@gmail.com>
2016-06-14 15:29:01 -0400

    test template renders correctly

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
+swig.setDefaults({cache: false });
+app.engine('html',swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 
 app.listen(3000);
 
@@ -11,4 +17,18 @@ app.use(function(req, res, next){
 app.use('/special/', function(req, res, next){
   console.log("special area");
   next();
-})
+});
+
+app.use(function(req,res,next){
+  //swig.renderFile('views', obj, function(err,output) {
+  //console.log(output);
+  res.render('index',obj);
+  });
+
+
+var title = "Twitter Clone"
+var obj = {
+  title: "twitter clone",
+  people : [{name: "Gandalf"},{name: "Frodo"}, {name: "Hermione"}]
+}
+
