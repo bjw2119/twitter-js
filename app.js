c580c5fff41981f12a7841603c60039af7ee2cc1
Barry Weinberg <bjw2119@gmail.com>
2016-06-14 15:59:11 -0400

    tweetBank works

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var swig = require('swig');
+var index = require('./routes/');
+var _= require('lodash');
+var tweetBank = require('./tweetBank');
 var app = express();
 swig.setDefaults({cache: false });
 app.engine('html',swig.renderFile);
@@ -10,7 +13,9 @@ app.set('views', __dirname + '/views');
 app.listen(3000);
 
 app.use(function(req, res, next){
-  console.log(req.method + " " + req.url + " " + res.status);
+  // console.log(req.method + " " + req.url + " " + res.status);
+  var arr = [{a: 1, b:2}, {a:3, b:4}, {c:5, d:6}];
+  console.log(_.cloneDeep(arr));
   next();
 });
 
