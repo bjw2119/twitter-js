1544586bd3e7b8db60a7c1af40c9a997c33bc0ed
Barry Weinberg <bjw2119@gmail.com>
2016-06-14 18:03:14 -0400

    working on sockets

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 
 var swig = require('swig');
-var router = require('./routes/');
+var routes = require('./routes/');
 var _= require('lodash');
 var express = require ('express');
+var socketio = require('socket.io');
+
 
 var app = express();
 
@@ -13,5 +15,6 @@ app.engine('html',swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.listen(3000);
-app.use(router);
+var server = app.listen(3000);
+var io = socketio.listen(server);
+app.use(routes(io));
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,50 @@
 var express = require('express');
 var tweetBank = require('../tweetBank');
+var bodyParserFunc = require('body-parser');
 
+var bodyParser = bodyParserFunc.urlencoded({extended: false});
 var router = express.Router();
 
-router.use('/static', express.static('public'));
-
-router.get('/static/stylesheets/style.css', function (req,res,next) {
-  console.log(__dirname);
-  res.sendFile('/stylesheets/style.css');
 
 
-});
+module.exports = function(io){
+  router.use(bodyParser);
+  router.use('/static', express.static('public'));
 
-router.get('/users/:userName', function(req, res, next){
-  var tweets = tweetBank.find({name:req.params.userName});
-  //console.log(req.params.userName +"'s tweets: "+tweets)
-  res.render('index', {title: req.params.userName+"'s tweets", tweets:tweets});
+  router.get('/static/stylesheets/style.css', function (req,res,next) {
+    console.log(__dirname);
+    res.sendFile('/stylesheets/style.css');
 
 
-});
-
-router.get('/tweets/:id', function(req,res,next){
-  var tweets = tweetBank.find({id:req.params.id})
-  console.log(tweets);
-  res.render('index', {title: "Tweet #"+req.params.id, tweets:tweets})
-});
-
-router.use(function(req,res,next){
-  //swig.renderFile('views', obj, function(err,output) {
-  //console.log(output);
-  var tweets = tweetBank.list();
-
-  res.render('index', {title: "twitter clone", tweets:tweets});
   });
 
-module.exports = router;
+  router.get('/users/:userName', function(req, res, next){
+    var userName = req.params.userName.toString();
+    var tweets = tweetBank.find({name:userName});
+    //console.log(req.params.userName +"'s tweets: "+tweets)
+    res.render('index', {title: userName+"'s tweets", tweets:tweets, showForm:true, user: userName});
+
+
+  });
+
+  router.get('/tweets/:id', function(req,res,next){
+    var tweets = tweetBank.find({id:req.params.id})
+    console.log(tweets);
+    res.render('index', {title: "Tweet #"+req.params.id, tweets:tweets})
+  });
+
+  router.post('/tweets', function(req,res,next){
+    console.log(req.body);
+    tweetBank.add(req.body.name, req.body.text);
+    next();
+  });
+
+  router.use(function(req,res,next){
+    //swig.renderFile('views', obj, function(err,output) {
+    //console.log(output);
+    var tweets = tweetBank.list();
+
+    res.render('index', {title: "twitter clone", tweets:tweets, showForm: true});
+    });
+  return router;
+};
